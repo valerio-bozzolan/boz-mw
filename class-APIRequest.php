65c02e5b8c907c6d21b4deaebaa5854a24259f3a
Valerio Bozzolan <gnu@linux.it>
2017-12-18 22:38:35 +0100

    +POST

--- class-APIRequest.php
+++ class-APIRequest.php
@@ -9,17 +9,18 @@
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 # GNU Affero General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 class APIRequest {
-	const WAIT = 2;
-	const WAIT_DOS = 5;
+	const WAIT      = 0.2;
+	const WAIT_POST = 5;
+	const WAIT_DOS  = 5;
 
-	const VERSION = 0.2;
+	const VERSION = 0.3;
 
 	private $api;
 	private $data;
@@ -28,85 +29,131 @@ class APIRequest {
 
 	private $last = null;
 
+	private $latestHttpResponseHeaders = [];
+	private $cookies = [];
+
 	private $debug = false;
 
-	function __construct($api, $data, $args = [] ) {
+	public function __construct( $api, $data = [], $args = [] ) {
 		$this->api = $api;
 
-		$this->data = array_merge( [
+		$this->data = array_replace( [
 	                'maxlag'  =>  5,
 			'format'  => 'json'
-		], $data);
+		], $data );
 
-		$this->args = array_merge( [
-			'context' => false,
-			'method'  => 'GET'
+		$this->args = array_replace( [
+			'method'     => 'GET',
+			'wait'       => self::WAIT,
+			'wait-post'  => self::WAIT_POST,
+			'user-agent' => sprintf( 'Boz-MW APIRequest.php/%s', self::VERSION ),
+			'headers'    => []
 		], $args );
-
-		if( $this->args['hello'] ) {
-			self::hello( $this->args['hello'] );
-		}
 	}
 
-	static function factory( $api, $args ) {
+	public static function factory( $api, $args = [] ) {
 		return new self( $api, $args );
 	}
 
-	function lastfetch() {
+	public function lastfetch() {
 		return isset( $this->last ) ? $this->last : $this->fetch();
 	}
 
 	// https://www.wikidata.org/w/api.php?action=wbgetclaims&entity=Q38&format=jsonfm
-	function fetchFirstClaimValue($property) {
+	public function fetchFirstClaimValue($property) {
 		return @ $this->lastfetch()->claims->{$property}[0]->mainsnak->datavalue->value;
 	}
 
-	function hasNext() {
+	public function hasNext() {
 		return $this->continue !== false;
 	}
 
-	function setDebug( $debug ) {
+	public function setDebug( $debug ) {
 		$this->debug = $debug;
 		return $this;
 	}
 
-	function fetch( $wait = true, $data = null ) {
-		$data = $data ? $data : $this->data;
+	public function fetch( $data = [], $args = [] ) {
+		$data = array_replace( $this->data, $data );
+		$args = array_replace( $this->args, $args );
 
-		if( $wait ) {
-			self::log('INFO', "Waiting");
-			sleep(self::WAIT);
-		}
-		self::log('INFO', "Fetching");
-
+		// HTTP query using file_get_contents()
+		$url = $this->api;
 		$query = http_build_query( $data );
-
-		$context = $args['context'];
-		if( false === $context ) {
-			$context = [
-				'http' => [
-					'method' => $args['method']
-				]
-			];
+		$context = [
+			'http' => []
+		];
+		$context['http']['method'] = $args['method'];
+		if( $args['user-agent'] ) {
+			$args['headers'][] = self::header( 'User-Agent', $args['user-agent'] );
 		}
-		$result = json_decode( file_get_contents("{$this->api}?$query", false, $context) );
+
+		if( $this->haveCookies() ) {
+			$args['headers'][] = $this->getCookieHeader();
+		}
+		if( $query ) {
+			if( 'POST' === $args['method'] ) {
+				$args['headers'][] = self::header('Content-Type', 'application/x-www-form-urlencoded' );
+				$context['http']['content'] = $query;
+			} else {
+				$url .= "?$query";
+			}
+		}
+		if( $args['headers'] ) {
+			$context['http']['header'] = self::implodeHTTPHeaders( $args['headers'] );
+		}
 
 		if( $this->debug ) {
-			self::log('DEBUG', "{$this->api}?$query");
+			self::log('DEBUG', $url );
 		}
+
+		if( $args['wait'] ) {
+			self::log('INFO', sprintf(
+				"Waiting %.2f s...",
+				$args['wait']
+			) );
+			usleep( $args['wait'] * 1000000 );
+		}
+
+		self::log('INFO', sprintf(
+			"%s request",
+			$args['method']
+		) );
+
+		$stream_context = stream_context_create( $context );
+		$result = file_get_contents( $url, false, $stream_context );
+		$result = json_decode( $result );
+
+		// Here $http_response_header exists magically (PHP merda!)
+		$this->loadHTTPResponseHeaders( $http_response_header );
 
 		self::log('INFO', "Fetched");
 
-		if( isset( $next->error ) && $next->error->code === 'maxlag' ) {
-			self::log(WARN, "Lag! Waiting");
-			sleep(self::WAIT_DOS);
-			return $this->fetch(false, $data);
+		if( isset( $result->error ) ) {
+			if( 'maxlag' === $result->error->code ) {
+				self::log( WARN, "Lag!");
+				$args = array_replace( [
+					'wait' => self::WAIT_DOS
+				], $args );
+				return $this->fetch( $data , $args );
+			} else {
+				print_r( $result->error );
+				throw new Exception( "API error" );
+			}
 		}
 
 		return $this->last = $result;
 	}
 
-	function getNext( $wait = true ) {
+	public function post( $data = [], $args = [] ) {
+		$args = array_replace( [
+			'method' => 'POST',
+			'wait'   => $this->args['wait-post']
+		], $args );
+		return $this->fetch( $data, $args );
+	}
+
+	public function getNext() {
 		$data = $this->data;
 
 		if( $this->continue ) {
@@ -116,7 +163,7 @@ class APIRequest {
 			}
 		}
 
-		$next = $this->fetch($wait, $data);
+		$next = $this->fetch( $data );
 
 		$this->continue = isset( $next->continue )
 			? $next->continue
@@ -125,11 +172,101 @@ class APIRequest {
 		return $next;
 	}
 
-	static function hello( $wall ) {
-		ini_set('user_agent', $wall);
+	public function getLatestHTTPResponseHeaders() {
+		return $this->latestHttpResponseHeaders;
 	}
 
-	static function log( $type, $msg ) {
+	/**
+	 * Load HTTP response headers, filling cookies.
+	 */
+	private function loadHTTPResponseHeaders( $http_response_headers ) {
+		$this->latestHttpResponseHeaders = self::parseHTTPResponseHeaders( $http_response_headers );
+		if( isset( $this->latestHttpResponseHeaders['Set-Cookie'] ) ) {
+			foreach( $this->latestHttpResponseHeaders['Set-Cookie'] as $cookie ) {
+				$this->setRawCookie( $cookie );
+			}
+		}
+	}
+
+	/**
+	 * Group HTTP headers by keys.
+	 *
+	 * @return array
+	 */
+	private static function parseHTTPResponseHeaders( $http_response_headers ) {
+		$headers = [];
+		foreach( $http_response_headers as $header ) {
+			$header_parts = explode(':', $header, 2);
+			if( 2 === count( $header_parts ) ) {
+				list( $name, $value ) = $header_parts;
+				if( ! isset( $headers[ $name ] ) ) {
+					$headers[ $name ] = [];
+				}
+				$headers[ $name ][] = ltrim( $value );
+			}
+		}
+		return $headers;
+	}
+
+	public function haveCookies() {
+		return $this->cookies;
+	}
+
+	public function getCookies() {
+		return $this->cookies;
+	}
+
+	public function setCookie( $name, $value ) {
+		$this->cookies[ $name ] = $value;
+		return $this;
+	}
+
+	private function setRawCookie( $cookie ) {
+		$parts = explode(';', $cookie);
+		if( isset( $parts[0] ) ) {
+			$name_value = explode('=', $parts[0] );
+			if( 2 === count( $name_value ) ) {
+				list( $name, $value ) = $name_value;
+				$this->setCookie( $name, $value );
+			}
+		}
+	}
+
+	public function getCookieHeader() {
+		$cookies = [];
+		foreach( $this->getCookies() as $name => $value ) {
+			$cookies[] = urlencode( $name ) . '=' . urlencode( $value );
+		}
+		return self::headerRaw(
+			'Cookie',
+			implode( '; ', $cookies )
+		);
+	}
+
+	public function getHTTPCookies() {
+		return $this->cookies;
+	}
+
+	private function implodeHTTPHeaders( $headers ) {
+		if( ! $headers ) {
+			return null;
+		}
+		$s = '';
+		foreach( $headers as $header ) {
+			$s .= "$header\r\n";
+		}
+		return $s;
+	}
+
+	private static function header( $name, $value ) {
+		return self::headerRaw( $name, urlencode( $value ) );
+	}
+
+	private static function headerRaw( $name, $value ) {
+		return sprintf( '%s: %s', $name, $value );
+	}
+
+	private static function log( $type, $msg ) {
 		printf("# [%s] \t %s\n", $type, $msg);
 	}
 }

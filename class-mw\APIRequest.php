716a92751420dd79b9c2fc6daf1f9c4bc21c058d
Valerio Bozzolan <gnu@linux.it>
2017-12-19 13:08:44 +0100

    +mw/wb namespaces

--- class-mw\APIRequest.php
+++ class-mw\APIRequest.php
@@ -15,6 +15,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+# MediaWiki
+namespace mw;
+
 class APIRequest {
 	const WAIT      = 0.2;
 	const WAIT_POST = 5;

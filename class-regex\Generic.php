d9db8041197fc86fe4048b174638b1e5bc9772f6
Valerio Bozzolan <gnu@linux.it>
2018-01-18 02:43:06 +0100

    fixed wrong named capture group regex

--- class-regex\Generic.php
+++ class-regex\Generic.php
@@ -1,6 +1,6 @@
 <?php
 # Boz-MW - Another MediaWiki API handler in PHP
-# Copyright (C) 2017 Valerio Bozzolan
+# Copyright (C) 2017, 2018 Valerio Bozzolan
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -28,7 +28,11 @@ class Generic {
 		if( null === $group_name ) {
 			return $group;
 		}
-		return "(?P<$group_name>...)";
+		return "(?P<$group_name>$group)";
+	}
+
+	public function groupName( $group_name ) {
+		return '${' . $name . '}';
 	}
 
 	public static function spaceBurger( $s ) {

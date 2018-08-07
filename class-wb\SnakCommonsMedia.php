5e011a604ce60de7e6f1d3c7318fe42f6489dd9c
Valerio Bozzolan <bozzolan.valerio@educ.di.unito.it>
2018-08-07 12:01:47 +0200

    change class wb\SnakCommonsMedia in order to use wb\DataValueCommonsMedia

--- class-wb\SnakCommonsMedia.php
+++ class-wb\SnakCommonsMedia.php
@@ -1,6 +1,6 @@
 <?php
 # Boz-MW - Another MediaWiki API handler in PHP
-# Copyright (C) 2017 Valerio Bozzolan
+# Copyright (C) 2017, 2018 Valerio Bozzolan
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -19,7 +19,7 @@
 namespace wb;
 
 /**
- * A Snak for a Commons filename.
+ * A Snak for a Commons filename
  */
 class SnakCommonsMedia extends Snak {
 
@@ -32,7 +32,7 @@ class SnakCommonsMedia extends Snak {
 			'value',
 			$property,
 			DataType::COMMONS_MEDIA,
-			new DataValueString( $filename )
+			new DataValueCommonsMedia( $filename )
 		);
 	}
 }

067732aa1adb775c2ccf4d060f5d80a9f3d068ea
Valerio Bozzolan <gnu@linux.it>
2020-04-07 08:00:49 +0200

    update repository URL

--- include/class-network\HTTPRequest.php
+++ include/class-network\HTTPRequest.php
@@ -1,6 +1,6 @@
 <?php
 # Boz-MW - Another MediaWiki API handler in PHP
-# Copyright (C) 2017, 2018, 2019 Valerio Bozzolan
+# Copyright (C) 2017, 2018, 2019, 2020 Valerio Bozzolan
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -40,7 +40,7 @@ class HTTPRequest {
 	 *
 	 * @var string
 	 */
-	const REPO = 'https://github.com/valerio-bozzolan/boz-mw';
+	const REPO = 'https://gitpull.it/source/boz-mw/';
 
 	/**
 	 * Wait seconds before each GET request.

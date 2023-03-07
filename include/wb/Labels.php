9ad702b0c800d35dca98ac72b1127c59f0e78bab
Valerio Bozzolan <nsa+gitspam@succhia.cz>
2023-03-07 19:54:40 +0100

    Labels: fix constructor calling undefined add() method

--- include/wb/Labels.php
+++ include/wb/Labels.php
@@ -1,6 +1,6 @@
 <?php
 # Boz-MW - Another MediaWiki API handler in PHP
-# Copyright (C) 2017, 2018 Valerio Bozzolan
+# Copyright (C) 2017-2023 Valerio Bozzolan
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -35,7 +35,7 @@ class Labels {
 	 */
 	public function __construct( $labels = [] ) {
 		foreach( $labels as $label ) {
-			$this->add( $label );
+			$this->set( $label );
 		}
 	}
 

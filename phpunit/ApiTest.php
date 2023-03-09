9b5efb5f2b7ef82c3387a5029e7a478465dba8fa
Valerio Bozzolan <>
2023-03-09 08:49:44 +0100

    ApiTest.php: minor refactor

--- phpunit/ApiTest.php
+++ phpunit/ApiTest.php
@@ -10,35 +10,37 @@ final class ApiTest extends TestCase {
 	/**
 	 * Test the itwiki API with Category members
 	 */
-	public function testItWikiCategoryMembers() {
+	public function testItwikiQueryCategoryMembers() {
+
+		$limit = 3;
 
 		$queries =
 			itwiki()->createQuery( [
 				'action'  => 'query',
 				'list'    => 'categorymembers',
 				'cmtitle' => 'Categoria:Software con licenza GNU GPL',
-				'cmlimit' => 3,
+				'cmlimit' => $limit,
 			] );
 
 		$i = 0;
-
 		foreach( $queries as $query ) {
-
 			foreach( $query->query->categorymembers as $member ) {
 
 				$member->pageid;
 				$member->ns;
 				$member->title;
 
+				// count the pages
 				$i++;
 			}
 
-			// don't do other requests
+			// it's not necessary to do consecutive queries
+			// this is just a test on the first page
 			break;
 		}
 
-		// it should return just 3 elements for this series
-		$this->assertEquals( $i, 3 );
+		// check whenever we were able to find $limit pages
+		$this->assertEquals( $i, $limit );
 	}
 
 }

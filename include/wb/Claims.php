972f2dbd96c83c4d43dca5b4d7d9caacda8befcf
Valerio Bozzolan <nsa+gitspam@succhia.cz>
2023-04-25 00:18:51 +0200

    Claims#toData(): minor refactor

--- include/wb/Claims.php
+++ include/wb/Claims.php
@@ -1,6 +1,6 @@
 <?php
 # Boz-MW - Another MediaWiki API handler in PHP
-# Copyright (C) 2017, 2018 Valerio Bozzolan
+# Copyright (C) 2017-2023 Valerio Bozzolan, contributors
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
@@ -128,12 +128,13 @@ class Claims {
 	 * @return array
 	 */
 	public function toData() {
-		$all = $this->getAllGrouped();
-		foreach( $all as & $claims ) {
+		$data = [];
+		foreach( $this->getAllGrouped() as $prop => $claims ) {
+			$data[ $prop ] = [];
 			foreach( $claims as & $claim ) {
-				$claim = $claim->toData();
+				$data[ $prop ][] = $claim->toData();
 			}
 		}
-		return $all;
+		return $data;
 	}
 }
